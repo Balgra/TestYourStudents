b4e91b00055e6a69afa93a9a8df03137cd6119af
Balgra <andreibalgradean16@gmail.com>
2021-04-26 23:17:55 +0300

    Defining Database Tables

---
+++ backend/TestYourStudents.Core/Entities/QuestionResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestYourStudents.Core.Entities
+{
+    public class QuestionResponse : BaseEntity
+    {
+        [ForeignKey ("QuestionId")]
+        public Question Question { get; set; }
+        
+        public int QuestionId { get; set; }
+
+        public string Response { get; set; }
+        public string Feedback { get; set; }
+
+    }
+}
