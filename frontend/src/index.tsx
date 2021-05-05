3773a1afac821be52eb07fa2ec2491f8096ca70f
Adelin Chis <adelin.chis@theaccessgroup.com>
2021-05-05 23:59:47 +0300

    [40][41] login and register forms

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  applyPolyfills,
+  defineCustomElements as defineCustomElements_v3,
+} from "@tag/tag-components-v3/loader";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div
+      style={{ height: "100vh" }}
+      className="container-fluid d-flex flex-column"
+    >
+      <App />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+applyPolyfills().then(() => {
+  defineCustomElements_v3();
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
