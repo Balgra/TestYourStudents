ee33ca4db0eec2ab22811b828e160b4526604f06
Adelin-Gabriel Chiș <adelin.chis@student.upt.ro>
2021-05-16 09:52:50 +0000

    Merged PR 20: [58] Get questions for a quiz and start the session
    [58] Get questions for a quiz and start the session
    
    Related work items: #58

--- backend/TestYourStudents.API/Controllers/QuestionController.cs
+++ backend/TestYourStudents.API/Controllers/QuestionController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TestYourStudents.API.Requests;
+using TestYourStudents.API.Responses.Models;
 using TestYourStudents.Core.Entities;
 using TestYourStudents.EF.EFRepositories.Abstractions;
 
@@ -15,12 +18,54 @@ namespace TestYourStudents.API.Controllers
         private readonly IRepository<Quiz> _quizRepo;
         private readonly IRepository<Course> _courseRepo;
         private readonly IRepository<Question> _questionRepo;
+        private readonly IRepository<StudentEmail> _studentEmailRepo;
+        private readonly IRepository<QuizSession> _quizSessionRepo;
 
-        public QuestionController(IRepository<Quiz> quizRepo, IRepository<Course> courseRepo, IRepository<Question> questionRepo)
+        public QuestionController(IRepository<Quiz> quizRepo, IRepository<Course> courseRepo, 
+            IRepository<Question> questionRepo, IRepository<StudentEmail> studentEmailRepo, IRepository<QuizSession> quizSessionRepo)
         {
             _quizRepo = quizRepo;
             _courseRepo = courseRepo;
             _questionRepo = questionRepo;
+            _studentEmailRepo = studentEmailRepo;
+            _quizSessionRepo = quizSessionRepo;
+        }
+
+        [HttpPost("session")]
+        [Authorize(Roles = "Student")]
+        public async Task<IActionResult> StartSessionAndGetQuestions([FromQuery] int quizId, [FromRoute] int courseId)
+        {
+            var userEmail = User.Claims.Where(x => x.Type == ClaimTypes.Email).FirstOrDefault()?.Value;
+            var userId = User.Claims.Where(c => c.Type.Equals("userId"))?.FirstOrDefault()?.Value;
+
+            var quiz = await _quizRepo.AsDbSet()
+                .Where(q => q.CourseId == courseId && q.Id == quizId && q.VisibleForStudents)
+                .SingleOrDefaultAsync();
+
+            if (quiz is null)
+                return BadRequest("The quiz does not exist or is not visible for students!");
+
+            var course = await _studentEmailRepo.AsDbSet()
+                .Where(s => s.Email == userEmail)
+                .FirstOrDefaultAsync();
+            if (course is null)
+                return BadRequest("You are not enrolled to that course!");
+
+            var questions = await _questionRepo.AsDbSet().Where(q => q.QuizId == quizId).ToListAsync();
+
+            var session = new QuizSession()
+            {
+                StartTime = DateTime.Now,
+                QuizId = quizId,
+                StudentId = userId,
+                Created = DateTime.Now,
+                CreatedByUserId = userId
+            };
+            await _quizSessionRepo.AddAsync(session);
+            await _quizSessionRepo.SaveChangesAsync();
+
+            return Ok(questions);
+
         }
 
         [HttpPost]
